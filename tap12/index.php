32951d59d99e3350661a7ef2fa9c00cfe772de8e
Aidana27 <75001581+Aidana27@users.noreply.github.com>
2021-03-02 01:11:32 +0600

    Add files via upload

---
+++ tap12/index.php
@@ -0,0 +1,5 @@
+<?php
+   define ("MY_AGE", "17");
+
+   echo ' Менің % Жасым% !' . MY_AGE . '<br>' ;
+?>
---
+++ tap13/index.php
@@ -0,0 +1,7 @@
+<?php
+   $str = 'qwerty';
+
+	echo $str[0]; 
+	echo $str[1]; 
+	echo $str[2]; 
+?>
